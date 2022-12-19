7bfff8c8deed8908b78f6e47c5f5f582a1b1c3a1
Shuahao18 <114126437+Shuahao18@users.noreply.github.com>
2022-12-19 15:40:39 +0800

    Create forlLoopDec.c

---
+++ forlLoopDec.c
@@ -0,0 +1,24 @@
+
+// FOR LOOP DECREMENT
+// JOSHUA U. MEREDORES
+// BSIT 1F
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int i, j;
+
+int main() {
+    printf("\nEnter initial value x : ");
+    scanf("%d", &i);
+
+    printf("\nEnter conditional value y : ");
+    scanf("%d", &j);
+
+for(i; i>=j; i--) 
+{
+    printf("\n %d",i);
+}
+
+return 0;
+}
